7e1699c7cbbd56940a1c27feddd59b9b8db40161
Ricardo Pedroso <rmdpedroso@gmail.com>
2012-05-24 00:06:03 +0100

    trying to better style css's

--- styles/css.py
+++ styles/css.py
@@ -8,37 +8,37 @@ def keywords():
 def styles(faces):
     return {
     stc.STC_CSS_DEFAULT:
-     "fore:#99ff99,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_ATTRIBUTE:
      "fore:#0040c0,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_CLASS:
-     "fore:#FF0080,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#5f5fbb,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
     stc.STC_CSS_COMMENT:
-     "fore:#007f00,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#5fbb5f,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_DIRECTIVE:
-     "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#5f5fbb,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_DOUBLESTRING:
-     "fore:#7f00bf,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#bb5f5f,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_ID:
-     "fore:#800080,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#aa00aa,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_IDENTIFIER:
      "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
     stc.STC_CSS_IDENTIFIER2:
      "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
     stc.STC_CSS_IMPORTANT:
-     "fore:#ff0000,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#ffffff,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_OPERATOR:
-     "fore:#0f00bf,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#777777,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_PSEUDOCLASS:
      "fore:#7f00bf,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_SINGLESTRING:
-     "fore:#7f00bf,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#bb5f5f,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_TAG:
-     "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
+     "fore:#999f9f,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
     stc.STC_CSS_UNKNOWN_IDENTIFIER:
-     "fore:#ff0000,back:%(back)s,face:%(helv)s,size:%(size)d,bold" % faces,
+     "fore:#999999,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_UNKNOWN_PSEUDOCLASS:
-     "fore:#ff0000,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
+     "fore:#aa0000,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
     stc.STC_CSS_VALUE:
         "fore:#ff00ff,back:%(back)s,face:%(helv)s,size:%(size)d" % faces,
 }
